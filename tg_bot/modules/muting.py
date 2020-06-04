9a1c9cbf7942a952213e5fd591da5346153ebf1c
mrflyman <66253635+mrflyman@users.noreply.github.com>
2020-06-04 19:07:31 +0300

    Create muting.py

---
+++ tg_bot/modules/muting.py
@@ -0,0 +1,194 @@
+import html
+from typing import Optional, List
+
+from telegram import Message, Chat, Update, Bot, User
+from telegram.error import BadRequest
+from telegram.ext import CommandHandler, Filters
+from telegram.ext.dispatcher import run_async
+from telegram.utils.helpers import mention_html
+
+from tg_bot import dispatcher, LOGGER
+from tg_bot.modules.helper_funcs.chat_status import bot_admin, user_admin, is_user_admin, can_restrict
+from tg_bot.modules.helper_funcs.extraction import extract_user, extract_user_and_text
+from tg_bot.modules.helper_funcs.string_handling import extract_time
+from tg_bot.modules.log_channel import loggable
+
+
+@run_async
+@bot_admin
+@user_admin
+@loggable
+def mute(bot: Bot, update: Update, args: List[str]) -> str:
+    chat = update.effective_chat  # type: Optional[Chat]
+    user = update.effective_user  # type: Optional[User]
+    message = update.effective_message  # type: Optional[Message]
+
+    user_id = extract_user(message, args)
+    if not user_id:
+        message.reply_text("Sessize almak için bana bir kullanıcı adı vermeniz veya susturulması gereken birine yanıt vermeniz gerekir.")
+        return ""
+
+    if user_id == bot.id:
+        message.reply_text("susmayacağım!")
+        return ""
+
+    member = chat.get_member(int(user_id))
+
+    if member:
+        if is_user_admin(chat, user_id, member=member):
+            message.reply_text("Bir yöneticinin konuşmasını engelleyemiyorum diye korkuyorum!")
+
+        elif member.can_send_messages is None or member.can_send_messages:
+            bot.restrict_chat_member(chat.id, user_id, can_send_messages=False)
+            message.reply_text("ağzına verdim konuşamaz")
+            return "<b>{}:</b>" \
+                   "\n#MUTE" \
+                   "\n<b>Admin:</b> {}" \
+                   "\n<b>User:</b> {}".format(html.escape(chat.title),
+                                              mention_html(user.id, user.first_name),
+                                              mention_html(member.user.id, member.user.first_name))
+
+        else:
+            message.reply_text("Bu kullanıcı zaten sessiz!")
+    else:
+        message.reply_text("Bu kullanıcı sohbette yok!")
+
+    return ""
+
+
+@run_async
+@bot_admin
+@user_admin
+@loggable
+def unmute(bot: Bot, update: Update, args: List[str]) -> str:
+    chat = update.effective_chat  # type: Optional[Chat]
+    user = update.effective_user  # type: Optional[User]
+    message = update.effective_message  # type: Optional[Message]
+
+    user_id = extract_user(message, args)
+    if not user_id:
+        message.reply_text("Ya sesinin iptali için bana bir kullanıcı adı vermeniz veya sesi çözülecek birine yanıt vermeniz gerekir.")
+        return ""
+
+    member = chat.get_member(int(user_id))
+
+    if member.status != 'kicked' and member.status != 'left':
+        if member.can_send_messages and member.can_send_media_messages \
+                and member.can_send_other_messages and member.can_add_web_page_previews:
+            message.reply_text("Bu kullanıcı zaten konuşma hakkına sahiptir.")
+        else:
+            bot.restrict_chat_member(chat.id, int(user_id),
+                                     can_send_messages=True,
+                                     can_send_media_messages=True,
+                                     can_send_other_messages=True,
+                                     can_add_web_page_previews=True)
+            message.reply_text("Unmuted!")
+            return "<b>{}:</b>" \
+                   "\n#UNMUTE" \
+                   "\n<b>Admin:</b> {}" \
+                   "\n<b>User:</b> {}".format(html.escape(chat.title),
+                                              mention_html(user.id, user.first_name),
+                                              mention_html(member.user.id, member.user.first_name))
+    else:
+        message.reply_text("Bu kullanıcı bile sohbet değil, onları açmak onları daha fazla konuşmak yapmaz "
+                           "zaten yapmak!")
+
+    return ""
+
+
+@run_async
+@bot_admin
+@can_restrict
+@user_admin
+@loggable
+def temp_mute(bot: Bot, update: Update, args: List[str]) -> str:
+    chat = update.effective_chat  # type: Optional[Chat]
+    user = update.effective_user  # type: Optional[User]
+    message = update.effective_message  # type: Optional[Message]
+
+    user_id, reason = extract_user_and_text(message, args)
+
+    if not user_id:
+        message.reply_text("Bir kullanıcıdan bahsediyor gibi görünmüyorsunuz.")
+        return ""
+
+    try:
+        member = chat.get_member(user_id)
+    except BadRequest as excp:
+        if excp.message == "User not found":
+            message.reply_text("I can't seem to find this user")
+            return ""
+        else:
+            raise
+
+    if is_user_admin(chat, user_id, member):
+        message.reply_text("I really wish I could mute admins...")
+        return ""
+
+    if user_id == bot.id:
+        message.reply_text("susmayacağım?")
+        return ""
+
+    if not reason:
+        message.reply_text("Bu kullanıcıyı sessize almak için bir zaman belirtmediniz!")
+        return ""
+
+    split_reason = reason.split(None, 1)
+
+    time_val = split_reason[0].lower()
+    if len(split_reason) > 1:
+        reason = split_reason[1]
+    else:
+        reason = ""
+
+    mutetime = extract_time(message, time_val)
+
+    if not mutetime:
+        return ""
+
+    log = "<b>{}:</b>" \
+          "\n#TEMP MUTED" \
+          "\n<b>Admin:</b> {}" \
+          "\n<b>User:</b> {}" \
+          "\n<b>Time:</b> {}".format(html.escape(chat.title), mention_html(user.id, user.first_name),
+                                     mention_html(member.user.id, member.user.first_name), time_val)
+    if reason:
+        log += "\n<b>Reason:</b> {}".format(reason)
+
+    try:
+        if member.can_send_messages is None or member.can_send_messages:
+            bot.restrict_chat_member(chat.id, user_id, until_date=mutetime, can_send_messages=False)
+            message.reply_text("çeneni kapaman için bir sebeb {}!".format(time_val))
+            return log
+        else:
+            message.reply_text("Bu kullanıcı zaten sessiz.")
+
+    except BadRequest as excp:
+        if excp.message == "Reply message not found":
+            # Do not reply
+            message.reply_text("çeneni kapaman için bir sebeb {}!".format(time_val), quote=False)
+            return log
+        else:
+            LOGGER.warning(update)
+            LOGGER.exception("ERROR muting user %s in chat %s (%s) due to %s", user_id, chat.title, chat.id,
+                             excp.message)
+            message.reply_text("Kahretsin, o kullanıcıyı sessize alamıyorum..")
+
+    return ""
+
+
+__help__ = """
+*sadece yönetici:*
+ - /mute <kullanıcı adı>: susturur
+ - /tmute <kullanıcı adı> (dakika/saat/gün): zamanlı susturur
+ - /unmute <kullnıcı adı>: sesini açar
+
+__mod_name__ = "sus"
+
+MUTE_HANDLER = CommandHandler("mute", mute, pass_args=True, filters=Filters.group)
+UNMUTE_HANDLER = CommandHandler("unmute", unmute, pass_args=True, filters=Filters.group)
+TEMPMUTE_HANDLER = CommandHandler(["tmute", "tempmute"], temp_mute, pass_args=True, filters=Filters.group)
+
+dispatcher.add_handler(MUTE_HANDLER)
+dispatcher.add_handler(UNMUTE_HANDLER)
+dispatcher.add_handler(TEMPMUTE_HANDLER)
