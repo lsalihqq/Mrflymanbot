0c29cf367cbe0401e1a651c40c7005eeaf9873f0
mrflyman <66253635+mrflyman@users.noreply.github.com>
2020-06-04 18:44:17 +0300

    Update afk.py

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -7,6 +7,7 @@ from telegram.ext import Filters, MessageHandler, run_async
 from tg_bot import dispatcher
 from tg_bot.modules.disable import DisableAbleCommandHandler, DisableAbleRegexHandler
 from tg_bot.modules.sql import afk_sql as sql
+from tg_bot.modules.users import get_user_id
 
 AFK_GROUP = 7
 AFK_REPLY_GROUP = 8
