5131e3249b67ec3c241470b0c10632193ffd07f0
mrflyman <66253635+mrflyman@users.noreply.github.com>
2020-06-04 19:02:04 +0300

    Create extras.py

---
+++ tg_bot/modules/extras.py
@@ -0,0 +1,281 @@
+import random, re
+from random import randint
+from telegram import Message, Update, Bot, User
+from telegram import MessageEntity
+from telegram.ext import Filters, MessageHandler, run_async
+
+from tg_bot import dispatcher
+from tg_bot.modules.disable import DisableAbleCommandHandler
+
+ABUSE_STRINGS = (
+    "Siktir git",
+    " git kendini becer",
+    "senin annen gey",
+    "senin baban lezbo",
+    "salak",
+    "Zenci",
+    "senin büyükannen trans",
+    "seni Çömez",
+"Arkanızı rahatlatın, korkacak bir şey yok, Tecavüz treni sonunda burada",
+    "Am",
+    "Eşcinsel burada",
+)
+
+EYES = [
+    ['⌐■', '■'],
+    [' ͠°', ' °'],
+    ['⇀', '↼'],
+    ['´• ', ' •`'],
+    ['´', '`'],
+    ['`', '´'],
+    ['ó', 'ò'],
+    ['ò', 'ó'],
+    ['⸌', '⸍'],
+    ['>', '<'],
+    ['Ƹ̵̡', 'Ʒ'],
+    ['ᗒ', 'ᗕ'],
+    ['⟃', '⟄'],
+    ['⪧', '⪦'],
+    ['⪦', '⪧'],
+    ['⪩', '⪨'],
+    ['⪨', '⪩'],
+    ['⪰', '⪯'],
+    ['⫑', '⫒'],
+    ['⨴', '⨵'],
+    ['⩿', '⪀'],
+    ['⩾', '⩽'],
+    ['⩺', '⩹'],
+    ['⩹', '⩺'],
+    ['◥▶', '◀◤'],
+    ['◍', '◎'],
+    ['/͠-', '┐͡-\\'],
+    ['⌣', '⌣”'],
+    [' ͡⎚', ' ͡⎚'],
+    ['≋'],
+    ['૦ઁ'],
+    ['  ͯ'],
+    ['  ͌'],
+    ['ළ'],
+    ['◉'],
+    ['☉'],
+    ['・'],
+    ['▰'],
+    ['ᵔ'],
+    [' ﾟ'],
+    ['□'],
+    ['☼'],
+    ['*'],
+    ['`'],
+    ['⚆'],
+    ['⊜'],
+    ['>'],
+    ['❍'],
+    ['￣'],
+    ['─'],
+    ['✿'],
+    ['•'],
+    ['T'],
+    ['^'],
+    ['ⱺ'],
+    ['@'],
+    ['ȍ'],
+    ['  '],
+    ['  '],
+    ['x'],
+    ['-'],
+    ['$'],
+    ['Ȍ'],
+    ['ʘ'],
+    ['Ꝋ'],
+    [''],
+    ['⸟'],
+    ['๏'],
+    ['ⴲ'],
+    ['◕'],
+    ['◔'],
+    ['✧'],
+    ['■'],
+    ['♥'],
+    [' ͡°'],
+    ['¬'],
+    [' º '],
+    ['⨶'],
+    ['⨱'],
+    ['⏓'],
+    ['⏒'],
+    ['⍜'],
+    ['⍤'],
+    ['ᚖ'],
+    ['ᴗ'],
+    ['ಠ'],
+    ['σ'],
+    ['☯']
+]
+
+MOUTHS = [
+    ['v'],
+    ['ᴥ'],
+    ['ᗝ'],
+    ['Ѡ'],
+    ['ᗜ'],
+    ['Ꮂ'],
+    ['ᨓ'],
+    ['ᨎ'],
+    ['ヮ'],
+    ['╭͜ʖ╮'],
+    [' ͟ل͜'],
+    [' ͜ʖ'],
+    [' ͟ʖ'],
+    [' ʖ̯'],
+    ['ω'],
+    [' ³'],
+    [' ε '],
+    ['﹏'],
+    ['□'],
+    ['ل͜'],
+    ['‿'],
+    ['╭╮'],
+    ['‿‿'],
+    ['▾'],
+    ['‸'],
+    ['Д'],
+    ['∀'],
+    ['!'],
+    ['人'],
+    ['.'],
+    ['ロ'],
+    ['_'],
+    ['෴'],
+    ['ѽ'],
+    ['ഌ'],
+    ['⏠'],
+    ['⏏'],
+    ['⍊'],
+    ['⍘'],
+    ['ツ'],
+    ['益'],
+    ['╭∩╮'],
+    ['Ĺ̯'],
+    ['◡'],
+    [' ͜つ']
+]
+
+EARS = [
+    ['q', 'p'],
+    ['ʢ', 'ʡ'],
+    ['⸮', '?'],
+    ['ʕ', 'ʔ'],
+    ['ᖗ', 'ᖘ'],
+    ['ᕦ', 'ᕥ'],
+    ['ᕦ(', ')ᕥ'],
+    ['ᕙ(', ')ᕗ'],
+    ['ᘳ', 'ᘰ'],
+    ['ᕮ', 'ᕭ'],
+    ['ᕳ', 'ᕲ'],
+    ['(', ')'],
+    ['[', ']'],
+    ['¯\\_', '_/¯'],
+    ['୧', '୨'],
+    ['୨', '୧'],
+    ['⤜(', ')⤏'],
+    ['☞', '☞'],
+    ['ᑫ', 'ᑷ'],
+    ['ᑴ', 'ᑷ'],
+    ['ヽ(', ')ﾉ'],
+    ['\\(', ')/'],
+    ['乁(', ')ㄏ'],
+    ['└[', ']┘'],
+    ['(づ', ')づ'],
+    ['(ง', ')ง'],
+    ['⎝', '⎠'],
+    ['ლ(', 'ლ)'],
+    ['ᕕ(', ')ᕗ'],
+    ['(∩', ')⊃━☆ﾟ.*'],
+]
+
+TOSS = (
+   "yazı",
+   "tura",
+)
+
+@run_async
+def roll(bot: Bot, update: Update):
+    update.message.reply_text(random.choice(range(1, 7)))
+	
+def toss(bot: Bot, update: Update):
+    update.message.reply_text(random.choice(TOSS))
+
+@run_async
+def abuse(bot: Bot, update: Update):
+    # reply to correct message
+    reply_text = update.effective_message.reply_to_message.reply_text if update.effective_message.reply_to_message else update.effective_message.reply_text
+    reply_text(random.choice(ABUSE_STRINGS))
+	
+@run_async
+def shrug(bot: Bot, update: Update):
+    # reply to correct message
+    reply_text = update.effective_message.reply_to_message.reply_text if update.effective_message.reply_to_message else update.effective_message.reply_text
+    reply_text("¯\_(ツ)_/¯")	
+	
+@run_async
+def bluetext(bot: Bot, update: Update):
+    # reply to correct message
+    reply_text = update.effective_message.reply_to_message.reply_text if update.effective_message.reply_to_message else update.effective_message.reply_text
+    reply_text("MAVİ METİN \ n TIKLAYIN \ n RENKLERE TAKILAN KABA BİR HAYVANIM")		
+
+@run_async
+def rlg(bot: Bot, update: Update):
+    # reply to correct message
+    eyes = random.choice(EYES)
+    mouth = random.choice(MOUTHS)
+    ears = random.choice(EARS)
+    repl = format(ears + eyes + mouth + eyes + ears)
+    update.message.reply_text(repl)
+	
+def decide(bot: Bot, update: Update):
+        r = randint(1, 100)
+        if r <= 65:
+            update.message.reply_text("eveet.")
+        elif r <= 90:
+            update.message.reply_text("hayır.")
+        else:
+            update.message.reply_text("belki.")
+            
+def table(bot: Bot, update: Update):
+            r = randint(1, 100)
+            if r <= 45:
+                update.message.reply_text("(╯°□°）╯彡 ┻━┻")
+            elif r <= 90:
+                update.message.reply_text("Çevirmek için yeni masa satın almak için para  gönderin")
+            else:
+                update.message.reply_text("Sen çaresiz ibne masa çevirmek yerine biraz iş yap.")
+		
+__help__ = """
+ - /shrug :omuz silkme XD olsun.
+ - /table : ve flip/unflipg :v.
+ - /decide :rastgele cevab evet/hayır/belki
+ - /toss : Bozuk Para
+ - /abuse : küfür
+ - /bluetext : check urself :V
+ - /roll : zar at
+ - /rlg : emoji yapar
+ - /zal <yazı> : fiyakalı yazı
+"""
+
+__mod_name__ = "eğlence"
+
+ROLL_HANDLER = DisableAbleCommandHandler("roll", roll)
+TOSS_HANDLER = DisableAbleCommandHandler("toss", toss)
+SHRUG_HANDLER = DisableAbleCommandHandler("shrug", shrug)
+BLUETEXT_HANDLER = DisableAbleCommandHandler("bluetext", bluetext)
+RLG_HANDLER = DisableAbleCommandHandler("rlg", rlg)
+DECIDE_HANDLER = DisableAbleCommandHandler("decide", decide)
+TABLE_HANDLER = DisableAbleCommandHandler("table", table)
+
+dispatcher.add_handler(ROLL_HANDLER)
+dispatcher.add_handler(TOSS_HANDLER)
+dispatcher.add_handler(SHRUG_HANDLER)
+dispatcher.add_handler(BLUETEXT_HANDLER)
+dispatcher.add_handler(RLG_HANDLER)
+dispatcher.add_handler(DECIDE_HANDLER)
+dispatcher.add_handler(TABLE_HANDLER)
