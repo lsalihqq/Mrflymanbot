8255bbfdc727fb218ebb4f9b937ae0797e1af5e0
mrflyman <66253635+mrflyman@users.noreply.github.com>
2020-06-04 18:53:44 +0300

    Create admin.py

---
+++ tg_bot/modules/admin.py
@@ -0,0 +1,246 @@
+import html
+from typing import Optional, List
+
+from telegram import Message, Chat, Update, Bot, User
+from telegram import ParseMode
+from telegram.error import BadRequest
+from telegram.ext import CommandHandler, Filters
+from telegram.ext.dispatcher import run_async
+from telegram.utils.helpers import escape_markdown, mention_html
+
+from tg_bot import dispatcher
+from tg_bot.modules.disable import DisableAbleCommandHandler
+from tg_bot.modules.helper_funcs.chat_status import bot_admin, can_promote, user_admin, can_pin
+from tg_bot.modules.helper_funcs.extraction import extract_user
+from tg_bot.modules.log_channel import loggable
+
+
+@run_async
+@bot_admin
+@can_promote
+@user_admin
+@loggable
+def promote(bot: Bot, update: Update, args: List[str]) -> str:
+    chat_id = update.effective_chat.id
+    message = update.effective_message  # type: Optional[Message]
+    chat = update.effective_chat  # type: Optional[Chat]
+    user = update.effective_user  # type: Optional[User]
+
+    user_id = extract_user(message, args)
+    if not user_id:
+        message.reply_text("mension one.... 🤷🏻‍♂.")
+        return ""
+
+    user_member = chat.get_member(user_id)
+    if user_member.status == 'administrator' or user_member.status == 'creator':
+        message.reply_text("Nasıl zaten bir yönetici olan birini tanıtmak için direnir?")
+        return ""
+
+    if user_id == bot.id:
+        message.reply_text("Kendimi tanıtamam! Benim için bunu yapmak için bir yönetici alın.")
+        return ""
+
+    # set same perms as bot - bot can't assign higher perms than itself!
+    bot_member = chat.get_member(bot.id)
+
+    bot.promoteChatMember(chat_id, user_id,
+                          can_change_info=bot_member.can_change_info,
+                          can_post_messages=bot_member.can_post_messages,
+                          can_edit_messages=bot_member.can_edit_messages,
+                          can_delete_messages=bot_member.can_delete_messages,
+                          # can_invite_users=bot_member.can_invite_users,
+                          can_restrict_members=bot_member.can_restrict_members,
+                          can_pin_messages=bot_member.can_pin_messages,
+                          can_promote_members=bot_member.can_promote_members)
+
+    message.reply_text("Terfi🧡")
+    return "<b>{}:</b>" \
+           "\n#PROMOTED" \
+           "\n<b>Admin:</b> {}" \
+           "\n<b>User:</b> {}".format(html.escape(chat.title),
+                                      mention_html(user.id, user.first_name),
+                                      mention_html(user_member.user.id, user_member.user.first_name))
+
+
+@run_async
+@bot_admin
+@can_promote
+@user_admin
+@loggable
+def demote(bot: Bot, update: Update, args: List[str]) -> str:
+    chat = update.effective_chat  # type: Optional[Chat]
+    message = update.effective_message  # type: Optional[Message]
+    user = update.effective_user  # type: Optional[User]
+
+    user_id = extract_user(message, args)
+    if not user_id:
+        message.reply_text("mension bir .... 🤷🏻.")
+        return ""
+
+    user_member = chat.get_member(user_id)
+    if user_member.status == 'creator':
+        message.reply_text("grubun yaratıcısına yasak.... 😬")
+        return ""
+
+    if not user_member.status == 'administrator':
+        message.reply_text("Terfi ettirilen bir şeyi düşüremez.!")
+        return ""
+
+    if user_id == bot.id:
+        message.reply_text("Kendimi düşüremem! Benim için bunu yapmak için bir yönetici alın.")
+        return ""
+
+    try:
+        bot.promoteChatMember(int(chat.id), int(user_id),
+                              can_change_info=False,
+                              can_post_messages=False,
+                              can_edit_messages=False,
+                              can_delete_messages=False,
+                              can_invite_users=False,
+                              can_restrict_members=False,
+                              can_pin_messages=False,
+                              can_promote_members=False)
+        message.reply_text("Başarıyla düşürüldü!")
+        return "<b>{}:</b>" \
+               "\n#DEMOTED" \
+               "\n<b>Admin:</b> {}" \
+               "\n<b>User:</b> {}".format(html.escape(chat.title),
+                                          mention_html(user.id, user.first_name),
+                                          mention_html(user_member.user.id, user_member.user.first_name))
+
+    except BadRequest:
+        message.reply_text("İntrem edilemedi. Ben yönetici olmayabilir, ya da yönetici durumu başka bir tarafından atandı "
+                           "kullanıcı, bu yüzden onlara hareket edemez!")
+        return ""
+
+
+@run_async
+@bot_admin
+@can_pin
+@user_admin
+@loggable
+def pin(bot: Bot, update: Update, args: List[str]) -> str:
+    user = update.effective_user  # type: Optional[User]
+    chat = update.effective_chat  # type: Optional[Chat]
+
+    is_group = chat.type != "private" and chat.type != "channel"
+
+    prev_message = update.effective_message.reply_to_message
+
+    is_silent = True
+    if len(args) >= 1:
+        is_silent = not (args[0].lower() == 'notify' or args[0].lower() == 'loud' or args[0].lower() == 'violent')
+
+    if prev_message and is_group:
+        try:
+            bot.pinChatMessage(chat.id, prev_message.message_id, disable_notification=is_silent)
+        except BadRequest as excp:
+            if excp.message == "Chat_not_modified":
+                pass
+            else:
+                raise
+        return "<b>{}:</b>" \
+               "\n#PINNED" \
+               "\n<b>Admin:</b> {}".format(html.escape(chat.title), mention_html(user.id, user.first_name))
+
+    return ""
+
+
+@run_async
+@bot_admin
+@can_pin
+@user_admin
+@loggable
+def unpin(bot: Bot, update: Update) -> str:
+    chat = update.effective_chat
+    user = update.effective_user  # type: Optional[User]
+
+    try:
+        bot.unpinChatMessage(chat.id)
+    except BadRequest as excp:
+        if excp.message == "Chat_not_modified":
+            pass
+        else:
+            raise
+
+    return "<b>{}:</b>" \
+           "\n#UNPINNED" \
+           "\n<b>Admin:</b> {}".format(html.escape(chat.title),
+                                       mention_html(user.id, user.first_name))
+
+
+@run_async
+@bot_admin
+@user_admin
+def invite(bot: Bot, update: Update):
+    chat = update.effective_chat  # type: Optional[Chat]
+    if chat.username:
+        update.effective_message.reply_text(chat.username)
+    elif chat.type == chat.SUPERGROUP or chat.type == chat.CHANNEL:
+        bot_member = chat.get_member(bot.id)
+        if bot_member.can_invite_users:
+            invitelink = bot.exportChatInviteLink(chat.id)
+            update.effective_message.reply_text(invitelink)
+        else:
+            update.effective_message.reply_text("Davet bağlantısına erişimim yok, izinlerimi değiştirmeyi deneyin!")
+    else:
+        update.effective_message.reply_text("Ben sadece süper gruplar ve kanallar için davet bağlantıları verebilir, üzgünüm!")
+
+
+@run_async
+def adminlist(bot: Bot, update: Update):
+    administrators = update.effective_chat.get_administrators()
+    text = "yönetici *{}*:".format(update.effective_chat.title or " ")
+    for admin in administrators:
+        user = admin.user
+        status = admin.status
+        name = "[{}](tg://user?id={})".format(user.first_name + " " + (user.last_name or ""), user.id)
+        if user.username:
+            name = "[{}](tg://user?id={})".format(user.first_name + (user.last_name or ""), user.id)
+        if status == "creator":
+            text += "\n 🔱 kurucu:"
+            text += "\n` • `{} \n\n 🔰 yetkili:".format(name)
+    for admin in administrators:
+        user = admin.user
+        status = admin.status
+        name = "[{}](tg://user?id={})".format(user.first_name + " " + (user.last_name or ""), user.id)
+        if user.username:
+            name = "[{}](tg://user?id={})".format(user.first_name + (user.last_name or ""), user.id)
+        if status == "administrator":
+            text += "\n` • `{}".format(name)
+    update.effective_message.reply_text(text, parse_mode=ParseMode.MARKDOWN)
+
+
+def __chat_settings__(chat_id, user_id):
+    return "you are *admin*: `{}`".format(
+        dispatcher.bot.get_chat_member(chat_id, user_id).status in ("administrator", "creator"))
+
+
+__help__ = """
+ - /adminlist: yetkili kişilerin listesi
+*sadece yöneteci:*
+ - /pin: mesajı sabitler
+ - /unpin: sabitlenen mesajı kaldırır
+ - /invitelink: davet linkini verir
+ - /promote: yetki verir yanıtlayarak
+ - /demote: yetki düşürür
+"""
+
+__mod_name__ = "yönetici"
+
+PIN_HANDLER = CommandHandler("pin", pin, pass_args=True, filters=Filters.group)
+UNPIN_HANDLER = CommandHandler("unpin", unpin, filters=Filters.group)
+
+INVITE_HANDLER = CommandHandler("invitelink", invite, filters=Filters.group)
+
+PROMOTE_HANDLER = CommandHandler("promote", promote, pass_args=True, filters=Filters.group)
+DEMOTE_HANDLER = CommandHandler("demote", demote, pass_args=True, filters=Filters.group)
+
+ADMINLIST_HANDLER = DisableAbleCommandHandler("adminlist", adminlist, filters=Filters.group)
+
+dispatcher.add_handler(PIN_HANDLER)
+dispatcher.add_handler(UNPIN_HANDLER)
+dispatcher.add_handler(INVITE_HANDLER)
+dispatcher.add_handler(PROMOTE_HANDLER)
+dispatcher.add_handler(DEMOTE_HANDLER)
+dispatcher.add_handler(ADMINLIST_HANDLER)
