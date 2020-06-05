d71d4d286184516e20faec2a9dfbb330d6abc353
mrflyman <66253635+mrflyman@users.noreply.github.com>
2020-06-05 16:21:40 +0300

    Update ud.py

--- tg_bot/modules/ud.py
+++ tg_bot/modules/ud.py
@@ -17,7 +17,7 @@ def ud(bot: Bot, update: Update):
 
 __help__ = """
  - /ud:{kelime} Aramak istediğiniz sözcüğü veya ifadeyi yazın. /ud telegram Word: Telegram Definition: Bir zamanlar popüler olan telekomünikasyon sistemi, gönderenin telgraf servisiyle iletişimkuracağı ve [telefon] üzerinden [mesajlarını] konuşacağı bir sistemdir. İletiyi alan kişi daha sonra teletip makinesi aracılığıyla alıcının [adresi] yakınındaki bir telgraf ofisine gönderir. İleti daha sonra adrese elden teslim edilir. 1851'den 2006'da hizmeti kesilene kadar, Western Union"
-""
+"""
 
 __mod_name__ = "ud"
   
