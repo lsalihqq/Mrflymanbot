bcc33e1306ee64bdb50daff9f95fb954f108b2fd
mrflyman <66253635+mrflyman@users.noreply.github.com>
2020-06-05 21:37:54 +0300

    Update and rename tg_bot/modules/emojis.py to tg_bot/modüller/emojis.py

--- tg_bot/modüller/emojis.py
+++ tg_bot/modüller/emojis.py
@@ -253,7 +253,7 @@ def hack(bot: Bot, update: Update):
     for x in range(EDIT_TIMES):
         msg.edit_text(hack_you[x%5])
         time.sleep(EDIT_SLEEP)
-    msg.edit_text('successful hacked')
+    msg.edit_text('hacklendi')
 
 
 
@@ -269,7 +269,7 @@ def love(bot: Bot, update: Update):
     for x in range(EDIT_TIMES):
         msg.edit_text(love_siren[x%5])
         time.sleep(EDIT_SLEEP)
-    msg.edit_text('പ്രണയം  😂 ')
+    msg.edit_text('seni seviyorum')
 
 
 
